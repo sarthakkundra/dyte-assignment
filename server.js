8b2b77d4c82edfb13dbec40be77a8266b0bef035
sarthakkundra <sarthakkundra21@gmail.com>
2021-07-17 09:19:14 +0530

    added some routes and initialised react app

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const connectDB = require('./config/db');
+const shortURL = require("./models/shortURL");
 const ShortUrl = require("./models/shortURL");
 
 const app = express();
 connectDB();
 app.listen(process.env.PORT || 3001);
 
+app.use(require('body-parser').urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send("Homepage");
+})
+
+app.post('/shorten', async (req, res) => {
+    console.log(req.body);
+    try {
+        await shortURL.create({ fullURL: req.body.fullURL })
+    } catch (error) {
+        console.error(error);
+    }
+
+})
+
+app.get('/:shortUrl', async (req, res) => {
+    try {
+        const rv = await shortURL.findOne({ shortURL: req.params.ShortUrl });        
+    } catch (error) {
+        console.error(error)
+    }
+      
+    if(rv === null) res.setStatus(404);
+    res.redirect(rv.fullURL);
 })
